ceb59c40d3569479e88bd0351000e1b869dc13e7
AdamMahdad <Adammahdad2006@hotmail.com>
2025-02-14 00:58:32 +0100

    banner change

--- index.php
+++ index.php
@@ -32,13 +32,12 @@ if (!isset($_SESSION['loggedIn']) && isset($_COOKIE['remember_me'])) {
   <link rel="stylesheet" href="./styling/home.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_bag" />
   <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
-/>
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
 </head>
 
 <body class="text-white">
-  <div class="h-screen w-full page-1 page-fade">
+  <div class="h-screen w-full page-1 page-fade flex flex-col relative">
     <!-- Navbar -->
     <nav class="w-full flex justify-center items-center">
 
@@ -77,7 +76,7 @@ if (!isset($_SESSION['loggedIn']) && isset($_COOKIE['remember_me'])) {
 
 
       <div class="flex items-center mt-4 py-4 absolute right-24">
-      <a href="#">
+        <a href="#">
           <span class="material-symbols-outlined">
             shopping_bag
           </span>
@@ -91,7 +90,7 @@ if (!isset($_SESSION['loggedIn']) && isset($_COOKIE['remember_me'])) {
 
 
 
-    <!--searchbar-->
+    <!-- searchbar -->
     <div class="flex items-center justify-center w-full mt-8">
       <Form method="get" class="w-full flex items-center justify-center">
         <input type="search" name="search" id="search" placeholder="Zoek naar een product"
@@ -100,123 +99,142 @@ if (!isset($_SESSION['loggedIn']) && isset($_COOKIE['remember_me'])) {
         </div> 
 
         <!-- Main Content -->
-        <div class=" items-center justify-center flex-col flex mt-8 w-full">
-          <h1 class="text-4xl mb-4">Populaire verkochte</h1>
+        <div class=" section-hero flex items-center justify-center h-full">
+        <div class="window-outline h-[89%] w-11/12">
+          <div class="window-main h-full">
+            <div class="window-bar">
+            </div>
+            <div class="window-content">
+              <div class="hero-text-content">
+                <div class="text-hero">
+                <span class="text-hero-serif"></span>
+                </div>
+                <div class="hero-about-grid">
+                  <div class="hero-about-null"></div>
+                  <div>
+                    <div class="text-hero-about">
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="window-grain"></div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
 
-          <div class="wrapper">
-          <div id="cards">
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
+
+  <div class="page-2 h-screen w-screen flex flex-col items-center">
+    <div>
+      <h1 class="text-5xl">Best sellers</h1>
+    </div>
+    <div class="wrapper flex justify-center items-center mt-10">
+      <div id="cards">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <i class="fa-duotone fa-apartment"></i>
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
                 <i class="fa-duotone fa-apartment"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-apartment"></i>
-                  <div class="card-info-title">
-                    <h3>Apartments</h3>
-                    <h4>Places to be apart. Wait, what?</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
-                <i class="fa-duotone fa-unicorn"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-unicorn"></i>
-                  <div class="card-info-title">
-                    <h3>Unicorns</h3>
-                    <h4>A single corn. Er, I mean horn.</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
-                <i class="fa-duotone fa-blender-phone"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-blender-phone"></i>
-                  <div class="card-info-title">
-                    <h3>Blender Phones</h3>
-                    <h4>These absolutely deserve to exist.</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
-                <i class="fa-duotone fa-person-to-portal"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-person-to-portal"></i>
-                  <div class="card-info-title">
-                    <h3>Adios</h3>
-                    <h4>See you...</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
-                <i class="fa-duotone fa-person-from-portal"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-person-from-portal"></i>
-                  <div class="card-info-title">
-                    <h3>I mean hello</h3>
-                    <h4>...over here.</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-content">
-              <div class="card-image">
-                <i class="fa-duotone fa-otter"></i>
-              </div>
-              <div class="card-info-wrapper">
-                <div class="card-info">
-                  <i class="fa-duotone fa-otter"></i>
-                  <div class="card-info-title">
-                    <h3>Otters</h3>
-                    <h4>Look at me, imma cute lil fella.</h4>
-                  </div>
+                <div class="card-info-title">
+                  <h3>Apartments</h3>
+                  <h4>Places to be apart. Wait, what?</h4>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-
-  </div>
-  <div class="swiper-button-next"></div>
-  <div class="swiper-button-prev"></div>
-  <!-- Paginatie -->
-  <div class="swiper-pagination"></div>
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <img src="https://plus.unsplash.com/premium_photo-1705004597770-40e4db5df8a6?q=80&w=2095&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
+                <div class="card-info-title">
+                  <h3>Unicorns</h3>
+                  <h4>A single corn. Er, I mean horn.</h4>
+                </div>
+              </div>
+            </div>
           </div>
-    </div>
-  <div class="page-2 h-screen w-screen bg-black">
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <i class="fa-duotone fa-blender-phone"></i>
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
+                <i class="fa-duotone fa-blender-phone"></i>
+                <div class="card-info-title">
+                  <h3>Blender Phones</h3>
+                  <h4>These absolutely deserve to exist.</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <i class="fa-duotone fa-person-to-portal"></i>
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
+                <i class="fa-duotone fa-person-to-portal"></i>
+                <div class="card-info-title">
+                  <h3>Adios</h3>
+                  <h4>See you...</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <i class="fa-duotone fa-person-from-portal"></i>
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
+                <i class="fa-duotone fa-person-from-portal"></i>
+                <div class="card-info-title">
+                  <h3>I mean hello</h3>
+                  <h4>...over here.</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-content">
+            <div class="card-image">
+              <i class="fa-duotone fa-otter"></i>
+            </div>
+            <div class="card-info-wrapper">
+              <div class="card-info">
+                <i class="fa-duotone fa-otter"></i>
+                <div class="card-info-title">
+                  <h3>Otters</h3>
+                  <h4>Look at me, imma cute lil fella.</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+
+    </div>
   </div>
 
 
-
+  <div class="h-screen"></div>
 
 
 
@@ -227,4 +245,4 @@ if (!isset($_SESSION['loggedIn']) && isset($_COOKIE['remember_me'])) {
   <script src="./javascript/script.js"></script>
 </body>
 
-</html>
+</html>h
